b0122648e91428d5815196ba5c41455154209c80
Eileen <kohe@purdue.edu>
2026-02-21 10:38:25 -0500

    parameterized *timecmp setter helper functional

--- temp_build/interrupts-exceptions/process_table.c
+++ temp_build/interrupts-exceptions/process_table.c
@@ -7,16 +7,13 @@
 int queue[5] = {1, 2, 3, 4, 5};
 int index = 0;
 
-//void set_timer(int valueh, int value) {
-//    *MTIMECMPH = *MTIME + valueh;
-//    *MTIMECMP = *MTIME + value;
-//}
+void set_timer(uint64_t value) {
+    //*MTIMECMPH = *MTIME + valueh;
+    *MTIMECMP = *MTIME + value;
+}
 
-// For this test: need to subtract 0xFE from flag to make flag = 1
-// Each handler should be called once. If not, flag will be wrong
 void meip_handler() {
     print("meip handling....");
-    //flag -= 0xE;
     *EXT_CLEAR = 0x1; // writing anything simulates clearing interrupt
     CSRW("mie", 0x088);
     index++;
@@ -24,17 +21,15 @@ void meip_handler() {
 }
 
 void mtip_handler() {
-    print("mtip handling....");
-    //flag -= 0xE0;
-    //set_timer(0, 5000); 
-    *MTIMECMP = *MTIME + 5000; // setting mtimecmph makes a very large value
+    print("mtip handling...."); 
+    //*MTIMECMP = *MTIME + 5000; // setting mtimecmp
+    set_timer(5000);
     index++;
     print("Handled. Index at %d", index); 
 }
 
 void msip_handler() {
     print("msip handling....");
-  //  flag -= 0x10;
     *MSIP = 0x0; // writing 0 clears this
     index++;
     print("Handled. Index at %d", index);
@@ -52,11 +47,5 @@ int main() {
 
     while (*MTIME < 0xFF);    
 	
-//    if (flag == 1) {
-//        test_pass("All vectored interrupts handled");
-//    } else {
-//        test_fail("Vectored interrupts not handled correctly");
-//    }
-
     return 0;
 }
