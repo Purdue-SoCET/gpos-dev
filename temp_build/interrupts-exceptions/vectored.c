e64b6bb937d2c8536a7c72d42e135ca2eb782801
Yun-Kai Chen <chen4606@asicfab.ecn.purdue.edu>
2026-02-21 11:00:19 -0500

    initial commit, s-mode not detected in waveform yet

--- temp_build/interrupts-exceptions/vectored.c
+++ temp_build/interrupts-exceptions/vectored.c
@@ -21,12 +21,19 @@ void msip_handler() {
     *MSIP = 0x0; // writing 0 clears this
 }
 
-int main() {
+void s_entry(void) {
+    setup_interrupt_s_vectored(s_mode_table, IE_STIE | IE_SSIE | IE_SEIE);
+    enable_interrupts_s();
+    setup_timer_interrupt();
+    enter_u_mode(user_main);
+
+    __builtin_unreachable();
+}
+
+int user_main() {
     *MTIMECMPH = 0x00;
     *MTIMECMP  = 0xFF;
 
-    setup_interrupt_m_vectored(vector_table, IE_MTIE | IE_MSIE | IE_MEIE);
-    enable_interrupts_m();
 
     *MSIP = 1;
     *EXT_SET = 1;
@@ -38,6 +45,39 @@ int main() {
     } else {
         test_fail("Vectored interrupts not handled correctly");
     }
+
+    /*while(1) {
+        asm volatile("wfi");
+    } */
+
+    return 0;
+
+}
+
+int main() {
+    
+    //kernel boot stuff later will get separated into kernel main or whatever
+    setup_interrupt_m_vectored(m_mode_table, IE_MTIE | IE_MSIE | IE_MEIE); //
+    enable_interrupts_m();
+    delegate_traps_to_s(0xFFFFFFFFu, 0xFFFFFFFFu); //hard code to delegate all delegable ints to s mode for now
+    enter_s_mode(s_entry);
+    
+    enter_u_mode(user_main);
+    
+    /**MTIMECMPH = 0x00;
+    *MTIMECMP  = 0xFF;
+
+
+    *MSIP = 1;
+    *EXT_SET = 1;
+
+    while(*MTIME < 0xFF);
+
+    if (flag == 1) {
+        test_pass("All vectored interrupts handled");
+    } else {
+        test_fail("Vectored interrupts not handled correctly");
+    } */
 
     return 0;
 }
