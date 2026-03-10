2358e62298e30b1bfb4428bd8c3e17b97d2c1fab
Amber <akkhauv@gmail.com>
2026-03-10 14:53:45 -0400

    isr stack fix

--- temp_build/kernel/kernel.c
+++ temp_build/kernel/kernel.c
@@ -31,7 +31,7 @@ void s_mode_boot(void) {
     return;
 }
 
-void m_mode_boot() { 
+void m_mode_boot(void* isr_stack_top, void* thread_stack_top) { 
     //kernel boot stuff later will get separated into kernel main or whatever
     setup_interrupts_m(m_mode_trap_entry, IE_MTIE | IE_MSIE | IE_MEIE); //
     print("m_mode table setup complete\n");
